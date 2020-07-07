f56c703c8e60807605d4cc10525f1f1680995628
Fran <Fran@DESKTOP-NO101JL>
2020-07-07 17:13:25 +0200

    first commit

---
+++ src/main/java/com/pacodam/springhbtutorial/SpringHbTutorialApplication.java
@@ -0,0 +1,15 @@
+package com.pacodam.springhbtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHbTutorialApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hola");
+        SpringApplication.run(SpringHbTutorialApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/pacodam/springhbtutorial/SpringHbTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacodam.springhbtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHbTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
